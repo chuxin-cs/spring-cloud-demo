0ca5c32ed56a47a0922100845d12b07bd1bda64c
chuxin <15115359055@139.com>
2023-10-23 09:43:38 +0800

    feat: 集成element-ui axios js-cookie~

--- src/store/index.js
+++ src/store/index.js
@@ -1,17 +1,10 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
+
+// 引入 modules
 
 export default new Vuex.Store({
-  state: {
-  },
-  getters: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
-})
+  modules: {},
+});
