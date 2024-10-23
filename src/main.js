3dea0aac472b84b4c8dbee2b4345f7fa164bb560
chuxin <15115359055@139.com>
2024-10-23 16:10:29 +0800

    refactor: 目录文件重新设计~

--- src/main.js
+++ src/main.js
@@ -1,15 +1,17 @@
 import Vue from "vue";
 import App from "./App";
-import store from "./store/index.js";
+import store from "./common/store/index.js";
 
 // uni-app 兼容 API
 import "./uni.promisify.adaptor";
-// 全局css
-import "./chuxin/styles/common.css";
-// 禁用生产报错提示
-Vue.config.productionTip = false;
 // uni-app 设置为 app
 App.mpType = "app";
+// 禁用生产报错提示
+Vue.config.productionTip = false;
+
+
+// 全局css
+import "./chuxin/styles/common.css";
 // 引入 uview 组件库
 import uView from "uview-ui";
 Vue.use(uView);
--- src/store/modules/system/user/service/index.js
+++
@@ -1,7 +0,0 @@
-import { login } from "@/chuxin/api";
-import { promisic } from "@/chuxin/utils/util";
-
-// 平台登录
-export function platformLogin() {
-  return promisic(login)({ provider: "weixin" });
-}
