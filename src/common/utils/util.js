37552419102926f6c072dfe6f718bd9b73cde420
chuxin <15115359055@139.com>
2024-10-23 15:59:34 +0800

    feat: 删除 外部的utils

--- src/common/utils/util.js
+++ src/common/utils/util.js
@@ -45,3 +45,17 @@ export function appendParamsToUrl(url, params) {
 
   return `${url}?${paramsStr}`;
 }
+
+export function debounce(func, delay) {
+  let timer; // 定时器
+  return function () {
+    let context = this; // 记录 this 值,防止在回调函数中丢失
+    let args = arguments; // 函数参数
+    //如果定时器存在，则清除定时器(如果没有,也没必要进行处理)
+    timer ? clearTimeout(timer) : null;
+    timer = setTimeout(() => {
+      // 防止 this 值变为 window
+      func.apply(context, args);
+    }, delay);
+  };
+}
